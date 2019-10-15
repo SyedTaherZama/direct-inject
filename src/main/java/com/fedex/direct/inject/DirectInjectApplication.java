9ace688ff02cfed228476f90c76604df61abf086
SyedTaherZama <syedtaher.zama@gmail.com>
2019-10-15 12:41:25 +0530

    added java and configuration files

---
+++ src/main/java/com/fedex/direct/inject/DirectInjectApplication.java
@@ -0,0 +1,13 @@
+package com.fedex.direct.inject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectInjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectInjectApplication.class, args);
+	}
+
+}
